6aa2261f7e05e97d67ab6b90a2a53b43de04c10a
sknqlegal <bernardoguerra109@gmail.com>
2025-08-10 00:19:01 -0300

    Refactor calculator operations to use switch-case structure and update .gitignore to exclude teste.cpp

--- projetos/calc/calcCompact.cpp
+++ projetos/calc/calcCompact.cpp
@@ -4,69 +4,79 @@
 #include <iomanip>
 
 int calcs() {
-    std::string operation;
-    std::cout << "select operation (lowercase)" << std::endl << "([A]ddition, [S]ubtraction, [D]ivision, [M]ultiplication, [P]ower, [SQRT] square root, [E]xit)" << std::endl;
+    char operation;
+    std::cout << "select operation (lowercase)" << std::endl << "([A]ddition, [S]ubtraction, [D]ivision, [M]ultiplication, [P]ower, [S (highercase)] square root, [E]xit)" << std::endl;
     std::cin >> operation;
     
     int num1{ 0 }, num2{ 0 };
-    if (operation == "a") {
-        std::cout << "type in 2 numbers: ";
-        std::cin >> num1 >> num2;
-        int result = num1 + num2;
-        std::cout << std::endl << "result: " << result << std::endl << std::endl;
-    }
-    else if (operation == "s") {
-        std::cout << "type in 2 numbers: ";
-        std::cin >> num1 >> num2;
-        int result = num1 - num2;
-        std::cout << std::endl << "result: " << result << std::endl << std::endl;
-    }
-    else if (operation == "d") {
-        std::cout << "type in 2 numbers: ";
-        std::cin >> num1 >> num2;
-        double result = (double)num1 / num2;
-        if (num2 == 0) {
-            std::cout << "can't divide by zero";
-        }
-        else {
-            if (result * 100 == round(result * 100)){
+    double result;
+    switch(operation){
+        case 'a':
+            std::cout << "type in 2 numbers: ";
+            std::cin >> num1 >> num2;
+            result = num1 + num2;
+            std::cout << std::endl << "result: " << result << std::endl << std::endl;
+            break;
+
+        case 's':
+            std::cout << "type in 2 numbers: ";
+            std::cin >> num1 >> num2;
+            result = num1 - num2;
+            std::cout << std::endl << "result: " << result << std::endl << std::endl;
+            break;
+        
+        case 'd':
+            std::cout << "type in 2 numbers: ";
+            std::cin >> num1 >> num2;
+            result = (double)num1 / num2;
+            if (num2 == 0) {
+                std::cout << "can't divide by zero";
+            }
+            else {
+                if (result * 100 == round(result * 100)){
+                    std::cout << std::endl << "result: " << result << std::endl << std::endl;
+                }
+                else{
+                    std::cout << std::endl << "result (approximate): " << std::setprecision(2) << result << std::endl << std::endl;
+                }
+            }
+            break;
+
+        case 'm':
+            std::cout << "type in 2 numbers: ";
+            std::cin >> num1 >> num2;
+            result = num1 * num2;
+            std::cout << std::endl << "result: " << result << std::endl << std::endl;
+            break;
+
+        case 'p':
+            std::cout << "type in 2 numbers (1st is base, 2nd is power): ";
+            std::cin >> num1 >> num2;
+            result = pow(num1, num2);
+            std::cout << std::endl << "result: " << result << std::endl << std::endl;
+            break;
+
+        case 'S': 
+            std::cout << "type in the number: ";
+            std::cin >> num1;
+            result = sqrt(num1);
+            if (num1 == pow(result, 2)) {
                 std::cout << std::endl << "result: " << result << std::endl << std::endl;
             }
             else{
                 std::cout << std::endl << "result (approximate): " << std::setprecision(2) << result << std::endl << std::endl;
             }
+            break;
+
+        case 'e':
+            return 0;
+            break;
+
+        default:
+            std::cerr << "unknown command" << std::endl << std::endl;
+            break;
         }
-    }
-    else if (operation == "m") {
-        std::cout << "type in 2 numbers: ";
-        std::cin >> num1 >> num2;
-        int result = num1 * num2;
-        std::cout << std::endl << "result: " << result << std::endl << std::endl;
-    }
-    else if (operation == "p") {
-        std::cout << "type in 2 numbers (1st is base, 2nd is power): ";
-        std::cin >> num1 >> num2;
-        double result = pow(num1, num2);
-        std::cout << std::endl << "result: " << result << std::endl << std::endl;
-    }
-    else if (operation == "sqrt") {
-        std::cout << "type in the number: ";
-        std::cin >> num1;
-        double result = sqrt(num1);
-        if (num1 == pow(result, 2)) {
-            std::cout << std::endl << "result: " << result << std::endl << std::endl;
-        }
-        else{
-            std::cout << std::endl << "result (approximate): " << std::setprecision(2) << result << std::endl << std::endl;
-        }
-    }
-    else if (operation == "e") {
-        return 0;
-    }
-    else{
-        std::cerr << "unknown command" << std::endl << std::endl;
         return 1;
-    }
 }
 
 int main() {
