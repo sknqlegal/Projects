69da2d9baecf4661f158075b9ee38ee980d68120
sknqlegal <bernardoguerra109@gmail.com>
2025-10-08 10:58:37 -0300

    Improved the looks of the application and fixed various logical issues.

--- projetos/baskhara/baskhara.cpp
+++ projetos/baskhara/baskhara.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
-using namespace std;
 
 class Bhaskhara{
     public:
@@ -26,14 +25,15 @@ class Bhaskhara{
             if (delta < 0){
                 delta *= -1; //multiplying it by -1 if negative prevents nan root value (line below)
             }
-            double deltaRoot = trunc(sqrt(delta)); //truncating cuz apparently some values for delta makes it so an irrational deltaRoot^2 == delta
+            double deltaRoot = trunc((sqrt(delta) * 10)) / 10; //truncating cuz apparently some values for delta makes it so an irrational deltaRoot^2 == delta
             
             if (b*b - 4*a*c < 0){
-                std::cout << "Your function has no real values. Show complex solutions? [y/n]: ";
+                std::cout << "Your function has no real values! Show complex solutions? [y/n]: " << std::endl;
                 std::string choice;
                 std::cin >> choice;
 
                 if (choice == "y"){
+                    std::cout << std::endl;
                     if (pow(deltaRoot, 2) == delta){ //if its a perfect root
                         
                         std::cout << "(" << -b << " + " << std::setprecision(2) << deltaRoot << "i" << ")" << " / " << (2 * a) << std::endl
@@ -42,39 +42,44 @@ class Bhaskhara{
                     else if (pow(deltaRoot, 2) != delta){
 
                         std::cout << "(" << -b << " + " << "sqrt(" << delta << ")i )" << " / " << (2 * a) << std::endl
-                                  << "(" << -b << " - " << "sqrt(" << delta << ")i )" << " / " << (2 * a) << std::endl;
+                                  << "(" << -b << " - " << "sqrt(" << delta << ")i )" << " / " << (2 * a) << std::endl
+                                  << std::endl;
 
                             std::string choice;
                             std::cout << "Approximate results? [y/n]:" << std::endl;
                             std::cin >> choice;
+                            std::cout << std::endl;
                             if (choice == "y"){
-                              std::cout << std::setprecision(2) << (-b + deltaRoot) / (2 * a) << std::endl
-                                        << std::setprecision(2) << (-b - deltaRoot) / (2 * a) << std::endl;
+                              std::cout << "(" << -b << " + " << std::setprecision(2) << deltaRoot << "i)" << " / " << (2 * a) << std::endl
+                                        << "(" << -b << " - " << std::setprecision(2) << deltaRoot << "i)" << " / " << (2 * a) << std::endl
+                                        << std::endl;
                             }
                     }
                 }
             }
 
             else if (b*b - 4*a*c == 0){
-                std::cout << -b / (2 * a) << std::endl;
+                std::cout << std::endl << -b / (2 * a) << std::endl << std::endl;
             }
 
             else if (b*b - 4*a*c > 0){
                 if (pow(deltaRoot, 2) == delta){
                         
-                        std::cout << (-b + deltaRoot) / (2 * a) << std::endl << (-b - deltaRoot) / (2 * a) << std::endl;
+                        std::cout << std::endl << (-b + deltaRoot) / (2 * a) << std::endl << (-b - deltaRoot) / (2 * a) << std::endl << std::endl;
                     }
                 if (pow(deltaRoot, 2) != delta){
                         
                         std::cout << "(" << -b << " + " << "sqrt(" << delta << "))" << " / " << (2 * a) << std::endl
-                                  << "(" << -b << " - " << "sqrt(" << delta << "))" << " / " << (2 * a) << std::endl;
+                                  << "(" << -b << " - " << "sqrt(" << delta << "))" << " / " << (2 * a) << std::endl
+                                  << std::endl;
 
                             std::string choice;
                             std::cout << "Approximate results? [y/n]:" << std::endl;
                             std::cin >> choice;
                             if (choice == "y"){
                               std::cout << std::setprecision(2) << (-b + deltaRoot) / (2 * a) << std::endl
-                                        << std::setprecision(2) << (-b - deltaRoot) / (2 * a) << std::endl;
+                                        << std::setprecision(2) << (-b - deltaRoot) / (2 * a) << std::endl
+                                        << std::endl;
                             }
                     }
             }
@@ -86,4 +91,4 @@ class Bhaskhara{
         Bhaskhara().start();
         while(Bhaskhara().calc()){}
         return 0;
-    }
+}
